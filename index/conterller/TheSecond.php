01ef34345d6e810e556d36a9da0a565709305003
LiuJieTan <1780973645@qq.com>
2018-02-01 21:35:27 +0800

    zheshi lalallalla

---
+++ index/conterller/TheSecond.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LiuJie
+ * Date: 2018/2/1
+ * Time: 下午9:28
+ */
+class TheSecond{
+    function index(){
+        echo 'TheSecond';
+    }
+}
---
+++ index/conterller/index.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: LiuJie
+ * Date: 2018/2/1
+ * Time: 下午9:27
+ */
+namespace text\index\controller;
+
+class Index{
+    function index(){
+        echo 'Index';
+    }
+}
